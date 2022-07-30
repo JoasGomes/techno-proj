5b61deb80c411869b53fb68c1af593aae097f55f
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-30 06:24:54 -0300

    Carrinho

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const vm = new Vue({
   data: {
     produtos: [],
     produto: false,
+    carrinho: [],
   },
   filters: {
     numeroPreco(valor) {
       return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL"})
+    }
+  },
+  computed: {
+    carrinhoTotal() {
+      let total = 0;
+      if(this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        })
+      }
+      return total;
     }
   },
   methods: {
@@ -23,9 +35,39 @@ const vm = new Vue({
       .then(r => {
         this.produto = r;
       })
+    },
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+    fecharModal({target, currentTarget}) {
+      if(target === currentTarget)
+        this.produto = false;
+    },
+    adicionarItem() {
+      this.produto.estoque--;
+      const {id, nome, preco} = this.produto;
+      this.carrinho.push({ id, nome, preco });
+    },
+    removerItem(index) {
+      this.carrinho.splice(index, 1)
+    },
+    checarLocalStorage() {
+      if(window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho)
+      }
+    }
+  },
+  watch: {
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho)
     }
   },
   created() {
     this.fetchProdutos();
+    this.checarLocalStorage();
   }
 })
