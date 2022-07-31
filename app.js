0f6320d4e23839a37ad624cfd577543d511a37ef
JoasGomes <jojoca.vitor777@gmail.com>
2022-07-31 06:48:20 -0300

    Alerta e Router

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
   },
   filters: {
     numeroPreco(valor) {
@@ -51,6 +53,7 @@ const vm = new Vue({
       this.produto.estoque--;
       const {id, nome, preco} = this.produto;
       this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`)
     },
     removerItem(index) {
       this.carrinho.splice(index, 1)
@@ -59,15 +62,34 @@ const vm = new Vue({
       if(window.localStorage.carrinho) {
         this.carrinho = JSON.parse(window.localStorage.carrinho)
       }
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      },1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if(hash) {
+        this.fetchProduto(hash.replace("#", ""));
+      }
     }
   },
   watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `# ${hash}`);
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
     }
   },
   created() {
     this.fetchProdutos();
+    this.router();
     this.checarLocalStorage();
   }
 })
