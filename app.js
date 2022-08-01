9ef282f42c8fca654483de0f3dd0601812dfdf14
JoasGomes <jojoca.vitor777@gmail.com>
2022-08-01 00:43:41 -0300

    Finalizando

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ const vm = new Vue({
         this.carrinho = JSON.parse(window.localStorage.carrinho)
       }
     },
+    compararEstoque() {
+      const items = this.carrinho.filter(item => {
+        if(item.id === this.produto.id){
+          return true;
+        }
+      })
+      this.produto.estoque = this.produto.estoque - items.length;
+    },
     alerta(mensagem) {
       this.mensagemAlerta = mensagem;
       this.alertaAtivo = true;
@@ -87,6 +95,9 @@ const vm = new Vue({
       document.title = this.produto.nome || "Techno";
       const hash = this.produto.id || "";
       history.pushState(null, null, `# ${hash}`);
+      if(this.produto) {
+        this.compararEstoque();
+      }
     },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho)
